3280e62ddc079ebf2aeccfa8a53a8805419887f5
wzdazj <43106310+wzdazj@users.noreply.github.com>
2018-11-22 12:46:46 +0800

    Add files via upload

---
+++ Homework9/Program1/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Crawler
+    {
+        private Hashtable urls = new Hashtable();
+        private int count = 0;
+        private List<string> currents;
+        static void Main(string[] args)
+        {
+            Crawler myCrawler = new Crawler();
+
+            string startUrl = "http://www.cnblogs.com/dstang2000/";
+            //if (args.Length>=1)
+            //{
+            //    startUrl = args[0];
+            //}
+            myCrawler.urls.Add(startUrl, false);
+            Stopwatch stopwatch = new Stopwatch();
+
+
+            stopwatch.Start();
+
+            myCrawler.Crawl();
+            //Parallel.Invoke(actions);
+
+            stopwatch.Stop();
+            Console.WriteLine("耗时" + stopwatch.ElapsedMilliseconds);
+        }
+
+        private void Crawl()
+        {
+            Console.WriteLine("开始爬行了");
+            while (true)
+            {
+                string current = null;
+                currents = new List<string>();         //存current数组
+                string[] html = new string[10];
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url])
+                    {
+                        continue;
+                    }
+                    currents.Add(url);                   //把url加到currents里
+                    current = url;
+                }
+
+                if (current == null || count > 30)
+                {
+                    break;
+                }
+
+                int tdNum = 10;                      //线程数
+                Thread[] tds = new Thread[tdNum];
+                for (int i = 0; i < tdNum; i++)
+                {
+                    if (i < currents.Count)
+                    {
+                        Console.WriteLine("爬行" + currents[i] + "页面");
+                        if(i<currents.Count) tds[i] = new Thread(() => { DownLoad(currents[i]); });
+                        urls[currents[i]] = true;
+                        count++;
+                        tds[i].Start();
+                        tds[i].Join();
+                    }
+
+                } 
+               
+            }
+            Console.WriteLine("爬行结束");
+        }
+
+
+        public void DownLoad(string url)
+        {
+            try
+            {
+               
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+
+
+                Parse(html);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void Parse(string html)
+        {
+            string strRef = @"(href|HREF)[]* = []*[""'][^""'#>]+[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\"', '#', ',', '>');
+                if (strRef.Length == 0)
+                {
+                    continue;
+                }
+
+                if (urls[strRef] == null)
+                {
+                    urls[strRef] = false;
+                }
+            }
+        }
+    }
+}
---
+++ Homework9/Program2/Program.cs
@@ -0,0 +1,135 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace homework9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  
+            string str1 = "drop table if exists client;" + "create table client(id int(11) primary key ,name varchar(20),phonenumber varchar(20));";
+            string str2 = "drop table if exists orders;" + "create table orders(orderName varchar(30), orderCount varchar(30));";
+            string str3 = "drop table if exists orderdetail;" + "create table orderdetail(goodprice varchar(20),goodamounts varchar(20));";
+            string url = "server = localhost; database =ccg ; User id = root; password = 11111";
+            MySqlConnection conn = new MySqlConnection(url);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(str1, conn);
+                cmd.ExecuteNonQuery();
+                cmd = new MySqlCommand(str2, conn);
+                cmd.ExecuteNonQuery();
+                cmd = new MySqlCommand(str3, conn);
+                cmd.ExecuteNonQuery();
+                //Console.WriteLine(cmd.CommandText);
+                if (conn.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("连接数据库成功");
+                }
+                else
+                {
+                    Console.WriteLine("连接数据库失败");
+                }
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                conn.Close();
+
+            }
+
+        }
+        //    public static List<Thread> ths = new List<Thread>();
+        //    private Hashtable urls = new Hashtable();
+        //    private Hashtable urls2 = new Hashtable();
+        //    private int count = 0;
+        //    private int count2 = 0;
+        //    public A a = null;
+        //    public delegate void ThreadRunCompleteEvent();
+
+        //    private object _lockObj = new object();
+        //    private int _downLoadCount = 10;
+        //    public int _alreadyRunCount = 0;
+        //    public ThreadRunCompleteEvent DownCompleted = null;
+
+
+        //    public void ThreadSample()
+        //    {
+        //        while (true)
+        //        {
+        //            string current = null;
+        //            foreach (string url in urls.Keys)
+        //            {
+        //                if ((bool)urls[url])
+        //                    continue;
+        //                current = url;
+        //            }
+
+        //            if (current == null || count > 10)
+        //                break;
+        //            DownCompleted += OnDownCompleted;
+        //        for (int i = 0; i < _downLoadCount; i++)
+        //        {
+        //            Thread thread = new Thread(DownLoad(current));
+        //            thread.Start();
+        //        }
+        //    }
+
+        //    string DownLoad(string current)
+        //    {
+        //        try
+        //        {
+        //            if (DownCompleted != null)
+        //                DownCompleted();
+        //            WebClient webclient = new WebClient();
+        //            webclient.Encoding = Encoding.UTF8;
+        //            string html = webclient.DownloadString(url);
+        //            string filename = "F:\\C#\\新建文件夹\\" + count.ToString();
+        //            File.WriteAllText(filename, html, Encoding.UTF8);
+        //            return html;
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            Console.WriteLine(e.Message);
+        //            return "";
+        //        }
+
+        //    }
+
+        //    private void OnDownCompleted()
+        //    {
+        //        lock (_lockObj)
+        //            _alreadyRunCount++;
+
+        //        if (_alreadyRunCount > _downLoadCount)
+        //        {
+        //            //全部下载完毕执行
+        //        }
+        //    }
+        //    static void Main(string[] args)
+        //    {
+        //        var stopWatch = new Stopwatch();
+        //        Program myCrawler = new Program();
+        //        string startUrl = "http://www.cnblogs.com/dstang2000/";
+        //        if (args.Length >= 1)
+        //            startUrl = args[0];
+        //        myCrawler.urls.Add(startUrl, false);
+        //        stopWatch.Start();
+        //        myCrawler.Crawl();
+        //    }
+    }
+}
